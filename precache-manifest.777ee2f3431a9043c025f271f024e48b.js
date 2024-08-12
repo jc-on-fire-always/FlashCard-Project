7afbafd6fd0a5a2146608279bf1248fb2ffa024e
jc-on-fire <147938885+jc-on-fire@users.noreply.github.com>
2024-08-12 15:39:47 +0530

    Updates

---
+++ precache-manifest.777ee2f3431a9043c025f271f024e48b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8fe5d24f071f2723b5e982a870d1027",
+    "url": "/FlashCard-Project/index.html"
+  },
+  {
+    "revision": "f43aab67ec06c64859e4",
+    "url": "/FlashCard-Project/static/css/main.ab3b7c3d.chunk.css"
+  },
+  {
+    "revision": "0b82510f369983c55220",
+    "url": "/FlashCard-Project/static/js/2.f01859b6.chunk.js"
+  },
+  {
+    "revision": "f43aab67ec06c64859e4",
+    "url": "/FlashCard-Project/static/js/main.2dbbe2a1.chunk.js"
+  },
+  {
+    "revision": "4b863f716cbd8803190e",
+    "url": "/FlashCard-Project/static/js/runtime~main.fd5995d6.js"
+  }
+]);
